fb9c1060ee657502502162c5a429036ffebb5af3
Ahmed Mahmud <ahmedmahmud12971@gmail.com>
2017-03-03 14:51:29 +0500

    Implemented Live Help Assistant

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000
+var code = process.env.CODE
 
 var clients = {};
 var admin = {};
@@ -19,7 +21,7 @@ app.use(express.static(__dirname + '/../public'));
 io.on('connection', function(socket) {
 
   socket.on('userJoined', function(name) {
-    if (name === "3/xC.r6") {
+    if (name === code) {
       admin = socket;
       socket.name = 'Mediks'
       socket.admin = true;
@@ -35,32 +37,37 @@ io.on('connection', function(socket) {
 
   socket.on('sendMessage', function(message) {
     if (socket.admin) {
+      clients[message.user].messages.push({ type: 'admin', message: message.message });
       socket.broadcast.to(clients[message.user].id).emit('messageRecieved', message.message);
-    } else if (Object.keys(admin).length) {
-      socket.broadcast.to(admin.id).emit('messageRecieved', message);
     } else {
-      socket.messages.push(message);
-      console.log(socket.messages)
+      if (admin.current === socket.user) {
+        socket.broadcast.to(admin.id).emit('messageRecieved', message);
+      }
+      socket.messages.push({ type: 'user', message: message });
     }
   });
 
   socket.on('fetchMessages', function(user) {
-    socket.emit('userMessages', clients[user] ? clients[user].messages : false);
+    if (socket.admin) {
+      admin.current = user;
+      socket.emit(admin.id).emit('userMessages', clients[user] ? clients[user].messages : false);
+    }
   });
 
   socket.on('userPage', function() {
+    admin.current = null;
     var users = fetchUsers();
     socket.emit('adminJoinedSuccessfully', users);
   });
 
-  // socket.on('disconnect', function() {
-  //   if (socket.user) {
-  //     delete clients[socket.user];
-  //   }
-  // });
+  socket.on('disconnect', function() {
+    if (socket.user) {
+      delete clients[socket.user];
+    }
+  });
 
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
   console.log('Listening on port 3000');
 });
